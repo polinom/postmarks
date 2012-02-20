21185f59bb799ea62f8f41dc71bedb6b573291a6
polinom <polinom100@gmail.com>
2012-02-20 14:09:47 -0600

    media files fix

--- settings.py
+++ settings.py
@@ -49,11 +49,11 @@ MEDIA_ROOT = '/home/polinom/webapps/static_media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://polinom.webfactional.com/media/'
+MEDIA_URL = '/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = 'http://polinom.webfactional.com/media/admin/'
+ADMIN_MEDIA_PREFIX = 'http://polynets.ca/media/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '7qgw!pl)@-8x)z-_=eh*omw$q#*37+h)cel-d*^-^hk2=u=zn^'
